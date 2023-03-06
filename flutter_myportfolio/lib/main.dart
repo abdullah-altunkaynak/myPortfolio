d5fba99d9b77bdd009067c6f979fd09246038ab8
Abdullah Altunkaynak <abdullah.altunkaynak@outlook.com>
2023-03-07 01:35:34 +0300

    added localization

--- flutter_myportfolio/lib/main.dart
+++ flutter_myportfolio/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -12,8 +15,18 @@ class MyPortfolio extends StatefulWidget {
   State<MyPortfolio> createState() => _MyPortfolioState();
 }
 
+enum languages { en, tr }
+
 class _MyPortfolioState extends State<MyPortfolio> {
+  @override
+  void initState() {
+    super.initState();
+    print("initState çalıştı");
+    readJson();
+  }
+
   bool _darkThemeBool = false;
+  languages selectedLanguage = languages.tr;
   IconData _iconDataThemeLight = Icons.wb_sunny;
   IconData _iconDataThemeDark = Icons.nights_stay;
   ThemeData _lightTheme =
@@ -22,6 +35,7 @@ class _MyPortfolioState extends State<MyPortfolio> {
       ThemeData(primarySwatch: Colors.pink, brightness: Brightness.dark);
   double deviceWidth = 0;
   double deviceHeight = 0;
+  var texts;
   @override
   Widget build(BuildContext context) {
     deviceWidth = MediaQuery.of(context).size.width;
@@ -40,19 +54,58 @@ class _MyPortfolioState extends State<MyPortfolio> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     mainImagesGrid(),
-                    mainRightSidePersonalInfo(),
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _darkThemeBool = !_darkThemeBool;
-                        });
-                      },
-                      icon: Icon(_darkThemeBool
-                          ? _iconDataThemeDark
-                          : _iconDataThemeLight),
-                    ),
+                    Container(
+                      width: deviceWidth - deviceWidth / 1.5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              languagesDropdownButton(),
+                              themeIconButton()
+                            ],
+                          ),
+                          mainRightSidePersonalInfo(),
+                        ],
+                      ),
+                    )
                   ],
-                )
+                ),
+                Center(
+                  child: Container(
+                    width: deviceWidth - 50,
+                    height: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Center(
+                  child: Container(
+                    width: deviceWidth / 1.3,
+                    child: Text(
+                      texts[selectedLanguage.name]["aboutMyselfShort"]
+                          .toString(),
+                      overflow: TextOverflow.clip,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: deviceWidth / 120 + deviceHeight / 120),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                        flex: 2,
+                        child: Column(
+                          children: [Text("")],
+                        )),
+                    Flexible(flex: 1, child: Container())
+                  ],
+                ),
               ],
             ),
           ),
@@ -164,7 +217,7 @@ class _MyPortfolioState extends State<MyPortfolio> {
         width: deviceWidth,
         color: _darkThemeBool ? Colors.deepPurple.shade900 : Colors.yellow,
         child: Text(
-          "Bu uygulama/site Flutter teknolojisi kullanılarak yapılmıştır.",
+          texts[selectedLanguage.name]["topInfoBanner"],
           style: TextStyle(fontSize: deviceWidth / 120 + deviceHeight / 120),
           textAlign: TextAlign.center,
         ),
@@ -175,7 +228,7 @@ class _MyPortfolioState extends State<MyPortfolio> {
   mainRightSidePersonalInfo() {
     return Center(
         child: Container(
-      height: deviceHeight,
+      height: deviceHeight - 100,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -187,16 +240,53 @@ class _MyPortfolioState extends State<MyPortfolio> {
               style: TextStyle(
                 fontSize: deviceWidth / 50 + deviceHeight / 50,
               )),
-          Text("Bilgisayar Mühendisi /",
+          Text("${texts[selectedLanguage.name]["computerEngineer"]} /",
               style: TextStyle(
                 fontSize: deviceWidth / 70 + deviceHeight / 70,
               )),
-          Text("Mobile Developer",
+          Text("${texts[selectedLanguage.name]["mobileDeveloper"]} /",
               style: TextStyle(
                 fontSize: deviceWidth / 70 + deviceHeight / 70,
               )),
         ],
       ),
     ));
+  }
+
+  void readJson() async {
+    final String response =
+        await rootBundle.loadString('assets/texts/texts.json');
+    final data = await json.decode(response);
+    print(data.toString());
+    setState(() {
+      texts = data;
+    });
+  }
+
+  languagesDropdownButton() {
+    return DropdownButton(
+      value: selectedLanguage,
+      items: languages.values.map((languages selectedLanguage) {
+        return DropdownMenuItem<languages>(
+            value: selectedLanguage,
+            child: Text(selectedLanguage.name.toString()));
+      }).toList(),
+      onChanged: (var newValue) {
+        setState(() {
+          selectedLanguage = newValue!;
+        });
+      },
+    );
+  }
+
+  themeIconButton() {
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          _darkThemeBool = !_darkThemeBool;
+        });
+      },
+      icon: Icon(_darkThemeBool ? _iconDataThemeDark : _iconDataThemeLight),
+    );
   }
 }
